a9c7889d0c8221970a34ffeea4a9c153acab0989
Jonathan Flowers <jflowers@opentable.com>
2022-12-24 09:55:01 +0000

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
+import Header from './Components/Header';
+import SpaceFrame from './Components/SpaceFrame';
+import Stage from './Components/Stage';
+import Content from './Components/Content';
 import './App.css';
+import './styles.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render(): React.ReactNode {
+      return(
+        <div>
+          <Header title="The Francis Bacon Sketchbook"/>
+          <SpaceFrame/>
+          <Stage/>
+          <Content/>
+        </div>       
+      )
+  }
 }
-
 export default App;
